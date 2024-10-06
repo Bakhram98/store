2f8c44a4ccf2003b47080b38820126a0a0b380a2
Bakhram Mavlyanov <bakhram.mavlyanov@mail.ru>
2024-10-06 14:23:46 +0300

    Добавил первую модель

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+    discribtion = models.TextField()
+
+
 # Create your models here.
